394c1028631bdb63aaec45bb697936fe4bc67e29
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-05 10:00:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config(object):
     
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", "23331203"))
     
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "05be4bb2e1e6806a2ffd23402079e23a")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -18,16 +18,16 @@ class Config(object):
     
     CHUNK_SIZE = int(128)
 
-    HTTP_PROXY = ""
+    HTTP_PROXY = "182.74.243.47:3128"
     
     MAX_MESSAGE_LENGTH = 4096
     
     PROCESS_MAX_TIMEOUT = 3600
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6077444526"))
 
     SESSION_NAME = "UploadLinkToFileBot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://botno1:renamebot@cluster0.mscve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     MAX_RESULTS = "50"
